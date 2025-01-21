9b7c4d39524045fcd9df6addadfe4d429d22afc2
maciekwoz <33222646+dobish@users.noreply.github.com>
2025-01-21 20:44:10 +0100

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import PiwikProProvider from "@piwikpro/next-piwik-pro";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <PiwikProProvider
+          containerId={process.env.NEXT_PUBLIC_CONTAINER_ID as string}
+          containerUrl={process.env.NEXT_PUBLIC_CONTAINER_URL as string}
+        >
+          {children}
+        </PiwikProProvider>
       </body>
     </html>
   );
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['@piwikpro/next-piwik-pro']
 };
 
 export default nextConfig;
