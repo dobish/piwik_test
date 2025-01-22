f558ee87bd33cbc72f0329fdeb6979c6e1061098
maciekwoz <33222646+dobish@users.noreply.github.com>
2025-01-22 21:25:06 +0100

    next blog template

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/piwik_test/' : '',
+  basePath: isProd ? '/piwik_test' : '',
+  output: 'export',
   transpilePackages: ['@piwikpro/next-piwik-pro']
 };
 
